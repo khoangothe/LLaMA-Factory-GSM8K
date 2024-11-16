42eb8892b1a356c42fa711efa73835f2bd68587b
khoangothe <ngotheanhkhoa@gmail.com>
2024-11-16 22:37:25 +0700

    add sample output

--- interview/harness_eval/gsm8k.sh
+++ interview/harness_eval/gsm8k.sh
@@ -4,4 +4,6 @@ lm_eval --model vllm --model_args pretrained=models/gsm8k-qwen \
   --tasks gsm8k,gsm8k_cot \
     --device cuda:0 \
     --wandb_args project=gsm8k_dashboard,name=gsm8k_sft\
+    --log_samples \
+    --output_path output/gsm8k-qwen \
     --batch_size auto
--- interview/harness_eval/qwen25coder.sh
+++ interview/harness_eval/qwen25coder.sh
@@ -3,6 +3,8 @@
 lm_eval --model vllm --model_args pretrained=Qwen/Qwen2.5-Coder-7B \
   --tasks gsm8k,gsm8k_cot \
     --device cuda:0 \
-    --wandb_args project=gsm8k_dashboard,name=gsm8k_sft\
+    --wandb_args project=gsm8k_dashboard,name=qwen25coder\
+    --log_samples \
+    --output_path output/qwen25coder \
     --batch_size auto
 
