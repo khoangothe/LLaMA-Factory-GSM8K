2ed8799e863e89f7169a279809bb073872a2ff3e
khoangothe <ngotheanhkhoa@gmail.com>
2024-11-16 22:02:20 +0700

    fix bug

--- interview/harness_eval/gsm8k.sh
+++ interview/harness_eval/gsm8k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-lm_eval --model vllm --model_args pretrained=models/gsm8k \
+lm_eval --model vllm --model_args pretrained=models/gsm8k-qwen \
   --tasks gsm8k \
     --device cuda:0 \
     --wandb_args project=gsm8k_dashboard,name=gsm8k_sft\
