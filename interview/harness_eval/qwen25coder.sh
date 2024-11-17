419951a8b065801ffd81b387e7ff30e2e729b03c
khoangothe <ngotheanhkhoa@gmail.com>
2024-11-17 22:18:33 +0700

    fix remove self consistency

--- interview/harness_eval/qwen25coder.sh
+++ interview/harness_eval/qwen25coder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 lm_eval --model vllm --model_args pretrained=Qwen/Qwen2.5-Coder-7B \
-  --tasks gsm8k,gsm8k_cot,gsm8k_cot_self_consistency,gsm8k_cot_zeroshot \
+  --tasks gsm8k,gsm8k_cot,gsm8k_cot_zeroshot \
     --device cuda:0 \
     --wandb_args project=gsm8k_dashboard_2,name=qwen25coder\
     --log_samples \
